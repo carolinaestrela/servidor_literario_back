8dd4535284d8fc3689f3bac0f635f1e514c67f72
Carolina Estrela <estrela.carolina@bol.com.br>
2019-07-13 23:11:18 -0300

    Adicionando script de start para desenvolvimento e um dedicado para o heroku

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const cors = require('../servidor_literario/node_modules/cors')
-const bodyParser = require('../servidor_literario/node_modules/body-parser')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const controller = require('./FilmesController')
 
-const servidor = express()
-servidor.use(cors())
-servidor.use(bodyParser.json())
+const servidor = express();
+servidor.use(cors());
+servidor.use(bodyParser.json());
 
 servidor.get('/filmes', (request, response) => {
     controller.getAll()
@@ -13,76 +13,79 @@ servidor.get('/filmes', (request, response) => {
         .catch(error => {
             console.error(error)
             response.sendStatus(500)
-        })
-})
+        });
+});
 
 servidor.get('/filmes/:id', (request, response) => {
-    const id = request.params.id
+    const id = request.params.id;
     controller.getById(id)
         .then(filme => {
             if (!filme) {
-                response.sendStatus(404)
+                response.sendStatus(404);
             } else {
-                response.sendStatus(filme)
+                response.sendStatus(filme);
             }
         })
         .catch(error => {
             if (error.name === "CastError") {
-                response.sendStatus(400)
+                response.sendStatus(400);
             } else {
-                response.sendStatus(500)
+                response.sendStatus(500);
             }
-        })
-})
+        });
+});
 
 servidor.post('/filmes', (request, response) => {
     controller.add(request.body)
         .then(filme => {
-            const _id = filme._id
-            response.send(_id)
+            const _id = filme._id;
+            response.send(_id);
         })
         .catch(error => {
             if (error.name === "ValidationError") {
-                response.sendStatus(400)
+                response.sendStatus(400);
             } else {
-                response.sendStatus(500)
+                response.sendStatus(500);
             }
-        })
-})
+        });
+});
 
 servidor.patch('/filmes/:id', (request, response) => {
-    const id = request.params.id
+    const id = request.params.id;
     controller.update(id, request.body)
         .then(filme => {
-            if (!filme) { response.sendStatus(404) }
-            else { response.sendStatus(filme) }
+            if (!filme) {
+                response.sendStatus(404);
+            } else {
+                response.sendStatus(filme);
+            }
         })
         .catch(error => {
             if (error.name === "MongoError" || error.name === "CastError") {
-                response.sendStatus(400)
+                response.sendStatus(400);
             } else {
-                response.sendStatus(500)
+                response.sendStatus(500);
             }
-        })
-})
+        });
+});
 
 servidor.delete('/filmes/:id', (request, response) => {
     controller.remove(request.params.id)
         .then(filme => {
             if (filme === null || filme === undefined) {
-                response.sendStatus(404)
+                response.sendStatus(404);
             } else {
-                response.sendStatus(204)
+                response.sendStatus(204);
             }
         })
         .catch(error => {
             if (error.name === "CastError") {
-                response.sendStatus(400)
+                response.sendStatus(400);
             } else {
-                response.sendStatus(500)
+                response.sendStatus(500);
             }
-        })
-})
+        });
+});
 
 servidor.get('/filmes/busca-por-livro/:livro', (request, response) => {
     controller.busca(request.params.livro)
@@ -100,7 +103,7 @@ servidor.get('/filmes/busca-por-livro/:livro', (request, response) => {
                 response.sendStatus(500);
             }
         });
-})
+});
 
 servidor.listen(process.env.PORT);
 console.log(`servidor_literario rodando na porta ${process.env.PORT}`);
